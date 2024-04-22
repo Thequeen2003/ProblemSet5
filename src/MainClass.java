08d0026417c2e2d2c863200b77a82613b13974a6
Alantis Green <112572029+Thequeen2003@users.noreply.github.com>
2024-04-22 16:17:59 -0400

    Add files via upload

---
+++ src/MainClass.java
@@ -0,0 +1,19 @@
+/**********************************************************************
+ * @file RandomWalk.java
+ * @brief: Sometimes "Game over" does not appear
+ * @author Maahla Fofack, Murren Kelly, Alantis Green
+ * @date: 11/30/2022
+ * @acknowledgement: Krohn Education How to Code Pong Game Video
+ ***********************************************************************/
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainClass {
+    public static int level;
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+
+    }
+
+}
